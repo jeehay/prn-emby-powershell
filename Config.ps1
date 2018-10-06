533b02ed06db2b0236fade051c04be32ef5a6377
jeehay <mohammad_m4jid@yahoo.co.uk>
2018-10-06 19:54:21 +0100

    Add files via upload

---
+++ Config.ps1
@@ -0,0 +1,161 @@
+﻿#Config file locations
+#cls
+$loc= $env:OneDrive+"\Powershell\New Way"
+
+$global:StudioName               = "$loc\StudioName.xml"
+$global:StudioMainCategory       = "$loc\StudioMainCategory.xml"
+$global:Category                 = "$loc\Category.xml"
+$global:StarsAlias               = "$loc\StarsAlias.xml"
+$global:StarCategory             = "$loc\StarCategory.xml"
+
+# Define the config variable
+$config = @{}
+
+if(-not (Test-Path $StudioName)) {
+    $config['StudioName'] = @{}
+    $config['StudioName'] | Export-Clixml $StudioName
+} else {
+    $config['StudioName'] = Import-Clixml $StudioName
+}
+
+# Check for and import configuration
+if(-not (Test-Path $StudioMainCategory)) {
+    $config['StudioMainCategory'] = @{}
+    $config['StudioMainCategory'] | Export-Clixml $StudioMainCategory
+} else {
+    $config['StudioMainCategory'] = Import-Clixml $StudioMainCategory
+}
+
+# Check for and import configuration
+if(-not (Test-Path $Category)) {
+    $config['Category'] = @{}
+    $config['Category'] | Export-Clixml $Category
+} else {
+    $config['Category'] = Import-Clixml $Category
+}
+
+# Check for and import the Stars Alias configuration
+if(-not (Test-Path $StarsAlias)) {
+    $config['StarsAlias'] = @{}
+    $config['StarsAlias'] | Export-Clixml $StarsAlias
+} else {
+    $config['StarsAlias'] = Import-Clixml $StarsAlias
+}
+
+# Check for and import configuration
+if(-not (Test-Path $StarCategory)) {
+    $config['StarCategory'] = @{}
+    $config['StarCategory'] | Export-Clixml $StarCategory
+} else {
+    $config['StarCategory'] = Import-Clixml $StarCategory
+}
+
+function Set-ConfigItem
+{
+    param($Key, $Value, $Scope)
+    
+    if(-not $Scope -or -not $Key)
+    {
+        throw 'Scope and key parameters must be provided'
+    }
+    # If the scope exists, add or set the item in the scope.
+    if($Config[$Scope])
+    {
+        $Config[$Scope][$Key] = $Value
+    }
+    else 
+    {
+        throw "Scope not available"
+    }
+    
+    # Also, update the config files
+    Write-Config
+}
+
+function Check-Set-ConfigItem
+{
+    param($Key, $Value, $Scope)
+    
+    if(-not $Scope -or -not $Key)
+    {
+        throw 'Scope and key parameters must be provided'
+    }
+    
+    #
+    if( (Get-ConfigItem -Scope $Scope -Key $Key) -ne $null ){
+        Get-ConfigItem -Scope $Scope -Key $Key
+    }
+    else{
+	    # If the scope exists, add or set the item in the scope.
+        if($Config[$Scope])
+        {
+           $Config[$Scope][$Key] = $Value
+        }
+       
+        Write-Config
+        Write-Host "Not found so write it"
+    }
+}
+
+function Get-ConfigItem
+{
+    param($Key, $Scope)
+
+    if(-not $Scope -or -not $Key)
+    {
+        throw 'Scope and key parameters must be provided'
+    }
+
+    # If the scope exists, add or set the item in the scope.
+    if($Config[$Scope])
+    {
+        $Config[$Scope][$Key]
+    }
+}
+
+function Remove-ConfigItem
+{
+    param($Key, $Scope)
+    if(-not $Scope)
+    {
+        throw 'Scope must be defined to remove an item.'
+    }
+    else
+    {
+        $config[$scope].Remove($Key)
+    }
+    Write-Config
+}
+
+function Get-Config
+{
+    param($Scope)
+    
+    if($Scope)
+    {
+        $config[$Scope].GetEnumerator() | 
+            Sort-Object Name
+    }
+    else
+    {
+        foreach($scope in $config.GetEnumerator())
+        {
+                Write-Verbose $scope.Name "scope" -ForegroundColor White -BackgroundColor Green
+                $scope.Value.GetEnumerator() | Sort-Object Name
+        }
+    }
+}
+
+function Write-Config
+{
+    
+    $config['StudioName']             | Export-Clixml -Path $StudioName
+    $config['StudioMainCategory']     | Export-Clixml -Path $StudioMainCategory
+    $config['Category']               | Export-Clixml -Path $Category
+    $config['StarCategory']           | Export-Clixml -Path $StarCategory
+    $config['StarsAlias']             | Export-Clixml -Path $StarsAlias
+}
+
+Export-ModuleMember -Function Set-ConfigItem,Get-ConfigItem,Remove-ConfigItem,Get-Config
+#,Read-Config
+
---
+++ movies.ps1
@@ -0,0 +1,83 @@
+﻿#cls
+$p = "A:\FM\[GammaEntertainment]\[GirlFriendsFilms]\Cheer Squad Sleepovers\21"
+$p2 = "A:\FM\``[GammaEntertainment``]\``[GirlFriendsFilms``]\Cheer Squad Sleepovers\21"
+cd "$p2"
+
+."D:\Powershell\New Way\Config.ps1"
+
+$regex = "\[([\w|\s*-]+)\]([\w|\s]+(?:\[[\w|\s]+\])?.*)\s*-\s*([\w|,|\s.]+)";
+$option = [System.StringSplitOptions]::RemoveEmptyEntries
+
+Get-ChildItem -LiteralPath $p -Filter *.mp4 |
+    ForEach-Object {
+        $FileName = $_.BaseName
+        $file_name = $FileName + ".nfo";
+        $groups = [regex]::Match($FileName, $regex).Captures.Groups
+
+          if($groups)
+          {
+                $studio = Get-ConfigItem -Scope 'StudioName' -Key $groups[1].Value.Trim()
+                $stud=$studio.Split(",", $option);
+                $studio2=$null
+                foreach ($stu in $stud)
+                {
+                   $studio2+= "<studio>$stu</studio>";
+                }
+
+                $Category = Get-ConfigItem -Scope 'Category' -Key (Get-ConfigItem -Scope 'StudioMainCategory' -Key $groups[1].Value.Trim())
+
+                $Title  = $groups[2].Value.Trim();
+
+                $star   = $groups[3].Value.Trim();
+                if($star)
+                {
+                    $st      = $star.Split(",", $option);
+                    $actor=$null
+                    $StarCategory_c=$Category+","+$studio
+                    foreach ($s in $st)
+                    {
+                       $sts = Get-ConfigItem -Scope 'StarsAlias' -Key $s.Trim()
+                       if ($sts){$sts=$sts}else{$sts=$s.Trim()}
+                       $c=(Get-ConfigItem -Scope 'StarCategory' -Key $sts);
+                       if($c){$StarCategory_c+=$c+','}
+
+                       $actor+= "<actor><name>$sts</name><type>Actor</type></actor>";
+                    }
+                }
+
+                $st_cat=$StarCategory_c.Split(",", $option);
+                $genre=$null
+                foreach ($c in $st_cat)
+                {
+                    $genre+="<genre>"+$c.Trim()+"</genre>";
+                }
+                
+                $t_name = ($Title+" - "+$star).Trim()
+                $file = "<?xml version=""1.0"" encoding=""utf-8"" standalone=""yes""?>
+                <movie>
+                    <customrating>XXX</customrating>
+                    <lockdata>true</lockdata>
+                    <rating>4.5</rating>
+                    <criticrating>5</criticrating>
+
+                    <mpaa>XXX</mpaa>
+                    <isuserfavorite>false</isuserfavorite>
+                    <playcount>0</playcount>
+                    <watched>false</watched>
+
+                <title>$t_name</title>
+        
+                $genre
+                $studio2
+                $actor
+           
+                </movie>
+                    ";
+
+                if(![System.IO.File]::Exists("$p`\$file_name"))
+                {
+                    New-Item -Path $p2 -Name "$file_name" -Type "file" -Value $file
+                    Write-Host "Created new file and text content added";
+                }
+          }
+}
